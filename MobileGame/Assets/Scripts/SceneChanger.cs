e8b46e68705e1493f7221eef28f7e60459266ece
jonesy15 <jonesy1567@outlook.com>
2018-11-30 23:05:55 +0000

    Initial Commit
    Initial Commit and Main Menu scene screated with play, options and quit buttons added that are clickable. Quit button exits on click, play takes you to the play scene and all buttons have hovering and clicked animations

---
+++ MobileGame/Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+
+    public void LoadScene(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Application has quit");
+    }
+}
---
+++ MobileGame/Assets/UIManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public void LoadScene(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
